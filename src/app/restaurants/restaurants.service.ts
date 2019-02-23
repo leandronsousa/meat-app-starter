46e09d10dcc61cf6a534951322e0a2da7a85945e
Leandro Nogueira <leandronsousa@gmail.com>
2019-02-23 10:55:36 -0300

    densenvolvimento continuo

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -1,18 +1,21 @@
-import { Restaurant } from "./restaurant/restaurant.model";
-import { MEAT_API } from "../app.api";
-import { Injectable } from "@angular/core";
-import { Http } from "@angular/http";
-import { Observable } from "rxjs/Observable";
+import { Restaurant } from './restaurant/restaurant.model';
+import { MEAT_API } from '../app.api';
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import { ErrorHandler } from '../app.error-handler';
+
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class RestaurantService {
 
-  constructor(private http : Http) {}
+  constructor(private http: Http) {}
 
-  restaurants() : Observable<Restaurant[]> {
-      return this.http.get(`${MEAT_API}/restaurants`)
-            .map(response => response.json())
+  restaurants(): Observable<Restaurant[]> {
+      return this.http.get(`${MEAT_API}/restaurants`).map(response => response.json())
+        .catch(ErrorHandler.handlerError);
   }
 
 }
