6019c652f98c899921b48c46b94241cc945226f8
Leandro Nogueira <leandronsousa@gmail.com>
2019-03-06 22:12:57 -0300

    cont 2

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,6 +5,13 @@ import { RatingComponent } from "./rating/rating.component";
 import { CommonModule } from "@angular/common";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
+/**
+ * Modulo compartilhado onde fica os componentes da aplicacao que podem 
+ * ser usado por outros componentes/modulos.
+ * CommonModule necessario para diretivas ngIf, ngFor ....
+ * Imports: Modulos necessarios para funcionar
+ * exports: tudo que sera exportado caso queira usar o modulo
+ */
 @NgModule({
     declarations: [InputComponent, RadioComponent, RatingComponent],
     imports: [CommonModule, FormsModule, ReactiveFormsModule],
