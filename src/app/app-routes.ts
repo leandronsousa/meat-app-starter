7726b48f9bd1918e5cc5007196adc94b6a2340e3
Leandro Nogueira <leandronsousa@gmail.com>
2019-03-31 23:27:32 -0300

    desenvolviment

--- src/app/app-routes.ts
+++ src/app/app-routes.ts
@@ -4,8 +4,8 @@ import { RestaurantsComponent } from "./restaurants/restaurants.component";
 import { RestaurantDetailComponent } from "./restaurant-detail/restaurant-detail.component";
 import { MenuComponent } from "./restaurant-detail/menu/menu.component";
 import { ReviewsComponent } from "./restaurant-detail/reviews/reviews.component";
-import { OrderComponent } from "./order/order.component";
 import { OrderSummaryComponent } from "./order-summary/order-summary.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
 
 export const ROUTES: Routes = [
     {path : '', component: HomeComponent},
@@ -19,5 +19,6 @@ export const ROUTES: Routes = [
     },
     {path: 'about', loadChildren: './about/about.module#AboutModule'},
     {path: 'order', loadChildren: './order/order.module#OrderModule'},
-    {path: 'order-summary', component : OrderSummaryComponent}
+    {path: 'order-summary', component : OrderSummaryComponent},
+    {path: '**', component: NotFoundComponent}
 ]
