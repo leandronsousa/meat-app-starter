3f7f488aedd18f680461b2a1ee318df0f4a219dc
Leandro Nogueira <leandronsousa@gmail.com>
2019-03-31 22:54:13 -0300

    desenvolvimento continuo

--- src/app/restaurant-detail/menu-item/menu-item.component.ts
+++ src/app/restaurant-detail/menu-item/menu-item.component.ts
@@ -1,14 +1,27 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { MenuItem } from './menu-item.model';
+import {trigger, state, style, transition, animate} from '@angular/animations';
+
 
 @Component({
   selector: 'mt-menu-item',
-  templateUrl: './menu-item.component.html'
+  templateUrl: './menu-item.component.html',
+  animations: [
+    trigger('menuItemAppeared',[
+      state('ready', style({opacity: 1})),
+      transition('void => ready', [//void para ready esta entrando na arvore de componentes aula 88
+        style({opacity: 0, transform: 'translateY(-20px)'}),
+        animate('300ms 0s ease-in')
+      ])
+    ])
+  ]
 })
 export class MenuItemComponent implements OnInit {
 
   @Input() menuItem: MenuItem;
   @Output() add = new EventEmitter();
+
+  menuItemState = 'ready';
 
   constructor() { }
 
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,14 @@
-import { NgModule } from "@angular/core";
+import { NgModule, ModuleWithProviders } from "@angular/core";
 import { InputComponent } from "./input/input.component";
 import { RadioComponent } from "./radio/radio.component";
 import { RatingComponent } from "./rating/rating.component";
 import { CommonModule } from "@angular/common";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { ShoppingCartService } from "app/restaurant-detail/shopping-cart/shopping-cart.service";
+import { RestaurantService } from "app/restaurants/restaurants.service";
+import { OrderService } from "app/order/order.service";
+import { SnackbarComponent } from 'app/shared/messages/snackbar/snackbar.component';
+import { NotificationService } from "./messages/notification.service";
 
 /**
  * Modulo compartilhado onde fica os componentes da aplicacao que podem 
@@ -13,11 +18,17 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
  * exports: tudo que sera exportado caso queira usar o modulo
  */
 @NgModule({
-    declarations: [InputComponent, RadioComponent, RatingComponent],
+    declarations: [InputComponent, RadioComponent, RatingComponent, SnackbarComponent],
     imports: [CommonModule, FormsModule, ReactiveFormsModule],
     exports: [InputComponent, RadioComponent, RatingComponent,
-            CommonModule, FormsModule, ReactiveFormsModule]
+            CommonModule, FormsModule, ReactiveFormsModule, SnackbarComponent]
 })
-export class SharedModule{
-
+export class SharedModule {
+    static forRoot(): ModuleWithProviders {
+        return {
+            ngModule: SharedModule,
+            providers: [ShoppingCartService, RestaurantService, 
+                OrderService, NotificationService]
+        }
+    }
 }
